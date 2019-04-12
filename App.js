55aa79c64428ccc395be682a814234ce4b0cad3b
Novan <novan.allanadi1211@gmail.com>
2019-04-13 01:56:09 +1000

    Base Amplify and react-native-paper test

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { Linking, Button, StyleSheet, Text, View } from 'react-native';
-import Auth from '@aws-amplify/auth';
-import Analytics from '@aws-amplify/analytics';
+import React from "react";
+import { Linking, StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-paper";
+import Auth from "@aws-amplify/auth";
+import Analytics from "@aws-amplify/analytics";
 
-import awsconfig from './aws-exports';
+import awsconfig from "./aws-exports";
 
 // retrieve temporary AWS credentials and sign requests
 Auth.configure(awsconfig);
@@ -14,33 +15,45 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.handleAnalyticsClick = this.handleAnalyticsClick.bind(this);
-    this.state = { resultHtml: <Text></Text>, eventsSent: 0 };
+    this.state = { resultHtml: <Text />, eventsSent: 0 };
   }
 
   handleAnalyticsClick() {
-    Analytics.record('AWS Amplify Tutorial Event')
-      .then((evt) => {
-        const url = 'https://' + awsconfig.aws_project_region + '.console.aws.amazon.com/pinpoint/home/?region=' + awsconfig.aws_project_region + '#/apps/' + awsconfig.aws_mobile_analytics_app_id + '/analytics/events';
-        let result = (
-          <View>
-            <Text>Event Submitted.</Text>
-            <Text>Events sent: {++this.state.eventsSent}</Text>
-            <Text style={styles.link} onPress={() => Linking.openURL(url)}>
-              View Events on the Amazon Pinpoint Console
-                </Text>
-          </View>
-        );
-        this.setState({
-          'resultHtml': result
-        });
+    Analytics.record("AWS Amplify Tutorial Event").then(evt => {
+      const url =
+        "https://" +
+        awsconfig.aws_project_region +
+        ".console.aws.amazon.com/pinpoint/home/?region=" +
+        awsconfig.aws_project_region +
+        "#/apps/" +
+        awsconfig.aws_mobile_analytics_app_id +
+        "/analytics/events";
+      let result = (
+        <View>
+          <Text>Event Submitted.</Text>
+          <Text>Events sent: {++this.state.eventsSent}</Text>
+          <Text style={styles.link} onPress={() => Linking.openURL(url)}>
+            View Events on the Amazon Pinpoint Console
+          </Text>
+        </View>
+      );
+      this.setState({
+        resultHtml: result,
       });
-  };
+    });
+  }
 
   render() {
     return (
       <View style={styles.container}>
         <Text>Welcome to your React Native App with Amplify!</Text>
-        <Button title="Generate Analytics Event" onPress={this.handleAnalyticsClick} />
+        <Button
+          icon="add-a-photo"
+          mode="contained"
+          onPress={() => this.handleAnalyticsClick}
+        >
+          Press me Moto Moto
+        </Button>
         {this.state.resultHtml}
       </View>
     );
@@ -50,11 +63,11 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   link: {
-    color: 'blue'
-  }
+    color: "blue",
+  },
 });
--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-/**
- * @format
- */
+import * as React from "react";
+import { AppRegistry } from "react-native";
+import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
+import App from "./App";
+import { name as appName } from "./app.json";
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "tomato",
+    accent: "yellow",
+  },
+};
 
-AppRegistry.registerComponent(appName, () => App);
+export default function Main() {
+  return (
+    <PaperProvider theme={theme}>
+      <App />
+    </PaperProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
