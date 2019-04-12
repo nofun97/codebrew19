72481cce80c695a833afd4523fa66c2b5a0a33fa
Novan <novan.allanadi1211@gmail.com>
2019-04-13 01:24:47 +1000

    Basic test for Amplify

--- App.js
+++ App.js
@@ -1,29 +1,47 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react';
+import { Linking, Button, StyleSheet, Text, View } from 'react-native';
+import Auth from '@aws-amplify/auth';
+import Analytics from '@aws-amplify/analytics';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import awsconfig from './aws-exports';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+// retrieve temporary AWS credentials and sign requests
+Auth.configure(awsconfig);
+// send analytics events to Amazon Pinpoint
+Analytics.configure(awsconfig);
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleAnalyticsClick = this.handleAnalyticsClick.bind(this);
+    this.state = { resultHtml: <Text></Text>, eventsSent: 0 };
+  }
+
+  handleAnalyticsClick() {
+    Analytics.record('AWS Amplify Tutorial Event')
+      .then((evt) => {
+        const url = 'https://' + awsconfig.aws_project_region + '.console.aws.amazon.com/pinpoint/home/?region=' + awsconfig.aws_project_region + '#/apps/' + awsconfig.aws_mobile_analytics_app_id + '/analytics/events';
+        let result = (
+          <View>
+            <Text>Event Submitted.</Text>
+            <Text>Events sent: {++this.state.eventsSent}</Text>
+            <Text style={styles.link} onPress={() => Linking.openURL(url)}>
+              View Events on the Amazon Pinpoint Console
+                </Text>
+          </View>
+        );
+        this.setState({
+          'resultHtml': result
+        });
+      });
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text>Welcome to your React Native App with Amplify!</Text>
+        <Button title="Generate Analytics Event" onPress={this.handleAnalyticsClick} />
+        {this.state.resultHtml}
       </View>
     );
   }
@@ -32,18 +50,11 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#fff',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'center',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  link: {
+    color: 'blue'
+  }
 });
