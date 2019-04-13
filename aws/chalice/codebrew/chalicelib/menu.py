84b60e92d87e106a2262f0d6e026cbc7f4c3c048
Novan <novan.allanadi1211@gmail.com>
2019-04-14 00:12:26 +1000

    check phrase checker and food extraction

--- aws/chalice/codebrew/chalicelib/menu.py
+++ aws/chalice/codebrew/chalicelib/menu.py
@@ -3,6 +3,8 @@ import pytesseract
 import cv2
 import os
 import re
+import requests
+import json
 
 img = cv2.imread("sample2.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -15,7 +17,6 @@ cv2.imwrite(filename, gray)
 # this holds the resulting string
 text = pytesseract.image_to_string(Image.open(filename))
 os.remove(filename)
-
 
 
 # print(type(text))
@@ -32,7 +33,47 @@ os.remove(filename)
 output = re.sub(r'\d+', '', text)
 
 
-#Method 2: separate into lines and remove empty strings
+# Method 2: separate into lines and remove empty strings
 output = output.split("\n")
 output = list(filter(None, output))
 print(output)
+
+# AppID 485558d3
+# AppKey c9cf20e81acf2f2a380fc79af83edd57
+
+
+def isFood(possibleFood):
+    url = "https://api.edamam.com/api/food-database/parser"
+    params = {
+        "app_id": "485558d3",
+        "app_key": "c9cf20e81acf2f2a380fc79af83edd57",
+        "ingr": possibleFood
+    }
+    response = requests.get(url, params)
+    data = response.json()
+    return len(data["parsed"]) > 0
+
+
+def extractPossibleFood(line):
+    line = line.lower()
+    url = "https://api.edamam.com/api/food-database/parser"
+    params = {
+        "app_id": "485558d3",
+        "app_key": "c9cf20e81acf2f2a380fc79af83edd57",
+        "ingr": line,
+        "nutrition-type": "logging"
+    }
+
+    response = requests.get(url, params)
+    data = response.json()
+    foods = []
+    for d in data["parsed"]:
+        food = d["food"]["label"]
+        if food.lower() in line:
+            foods.append(d["food"]["label"])
+    return foods
+
+
+if __name__ == "__main__":
+    print(isFood("Poached Egg"))
+    print(extractPossibleFood("Poached Egg $20 Fried Rice 30$"))
