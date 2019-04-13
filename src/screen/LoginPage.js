2a7222714b473c38b51a11c384e7e90dd50160b9
MauriceBernardo <maurice_tikus@hotmail.com>
2019-04-13 16:57:40 +1000

    added handle press for login option

--- src/screen/LoginPage.js
+++ src/screen/LoginPage.js
@@ -1,38 +1,58 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Icon } from "react-native";
-import { Button } from 'react-native-paper';
+import { Alert, StyleSheet, Text, View, Icon } from "react-native";
+import { Button } from "react-native-paper";
 
 export default class LoginPage extends Component {
   static navigationOption = {
-    tabBarVisible : false,
-  }
+    tabBarVisible: false,
+  };
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      showLogIn : true,
-    }
+      showLogIn: true,
+    };
   }
 
-  authorize(){
-    this.props.navigation.navigate("MainPage");
+  handlePress() {
+    this.setState(previousState => (
+      { showLogIn: !previousState.showLogIn }
+    ));
   }
 
   render() {
     return (
-      <View style ={styles.container}>
-        <Text style = {styles.appName}>Paragraph</Text>
-        <View style= {styles.option}>
-          <Button mode="text" disabled={this.state.showLogIn? true : false} onPress= {() => {}} color= 'black'> LOGIN </Button>
-          <Button mode="text" disabled={this.state.showLogIn? false : true} onPress= {() => {}} color= 'black'> SIGN IN </Button>
+      <View style={styles.container}>
+        <Text style={styles.appName}>Paragraph</Text>
+        <View style={styles.option}>
+          <Button
+            mode="text"
+            disabled={this.state.showLogIn}
+            onPress={this.handlePress.bind(this)}
+            color="black"
+          >
+            {" "}
+            LOGIN{" "}
+          </Button>
+          <Button
+            mode="text"
+            disabled={!this.state.showLogIn}
+            onPress= {this.handlePress.bind(this)}
+            color="black"
+          >
+            {" "}
+            SIGN IN{" "}
+          </Button>
         </View>
         {/* form component */}
-        <Button icon="add-a-photo" onPress={() => this.props.navigation.push("MainPage")}></Button>
+        <Button
+          icon="add-a-photo"
+          onPress={() => this.props.navigation.push("MainPage")}
+        />
       </View>
-    )
-  };
-};
-
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -40,16 +60,16 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "flex-start"
+    justifyContent: "flex-start",
   },
   appName: {
-    fontFamily: 'Open Sans',
+    fontFamily: "Open Sans",
     fontSize: 60,
-    color: 'black',
-    fontWeight: 'bold',
+    color: "black",
+    fontWeight: "bold",
   },
   option: {
-    flexDirection: 'row',
-    paddingTop: 25
-  }
+    flexDirection: "row",
+    paddingTop: 25,
+  },
 });
