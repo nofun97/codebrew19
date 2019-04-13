d30761ee428d8737f8984f4929b31bf76a47abc2
Jansen Kwong <j.kwong4@student.unimelb.edu.au>
2019-04-13 22:39:53 +1000

    add details

--- src/screen/DetailsPage.js
+++ src/screen/DetailsPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Image, Dimensions } from "react-native";
+import { StyleSheet, Text, View, Image, Dimensions, ScrollView } from "react-native";
 import { Button, TextInput } from "react-native-paper";
+import { Rating } from "react-native-elements";
 
 export default class DetailsPage extends Component {
   constructor(props) {
@@ -19,7 +20,7 @@ export default class DetailsPage extends Component {
     const imageWidth = dimensions.width;
 
     return (
-      <View style={styles.container}>
+      <ScrollView>
         <Image
           style={{ width: imageWidth, height: imageHeight }}
           source={{
@@ -27,7 +28,24 @@ export default class DetailsPage extends Component {
               "https://facebook.github.io/react-native/docs/assets/favicon.png",
           }}
         />
+      <View style={{marginLeft: 10, fontSize: "Bold"}}>
+        <Text style={styles.foodName}>Avacado Roll</Text>
+        <View>
+        <Rating
+                  imageSize={20}
+                  readonly
+                  startingValue={this.props.rating}
+                  style={{ paddingVertical: 10 }}
+                />
+        <Text style={styles.randomText}>4.6/5</Text>
+        </View>
+        <Text style= {styles.randomText}>$</Text>
+        <Text style= {styles.randomText}>1.5 km</Text>
+        <Text style= {styles.randomText}>Tokui Sushi</Text>
+        <Button icon="location-on" style={styles.button}>300 Lonsdale St, Melbourne, 3000, VIC</Button>
+        <Text style= {styles.randomText}>Rating</Text>
       </View>
+      </ScrollView>
     );
   }
 }
@@ -39,4 +57,13 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "flex-start",
   },
+  foodName: {
+    fontSize: 24
+  },
+  randomText: {
+    fontSize: 19
+  },
+  button: {
+    fontSize: 13
+  }
 });
